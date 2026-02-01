80e9ddcfc701536fe27cdbd073a7f7b807f715f8
Desmond Liu <desmondzyliu@gmail.com>
2026-02-01 17:18:10 +0100

    add desmond build command

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const desmondBuild = process.env.DESMOND_BUILD === 'true';
+const basePath = process.env.NODE_ENV === 'production' ? (desmondBuild ? '/gamejam2026' : '') : '';
+
 const nextConfig: NextConfig = {
   output: 'export',
+  basePath: basePath,
   distDir: process.env.NODE_ENV === 'production' ? 'out' : '.next',
   images: {
     unoptimized: true,
